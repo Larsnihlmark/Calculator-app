f976105221c59a45e71d5087fabdc6381ed15c80
Lars Nihlmark <larsnihlmark@gmail.com>
2021-11-18 12:11:14 +0100

    Button, functionality and styling CSS

---
+++ src/components/button.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./button.css";
+
+const isOperator = val => {
+    return !isNaN(val) || val === "." || val === "=";
+};
+
+const Button = props => (
+    <div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}>
+        {props.children}
+    </div>
+);
+
+export default Button;
