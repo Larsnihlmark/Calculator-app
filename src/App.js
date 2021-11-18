3051a49d135a7ff225ae4cd2784f707a0f8464a5
Lars Nihlmark <larsnihlmark@gmail.com>
2021-11-18 13:20:29 +0100

    onclick and Handle added mathjs module

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import Button from './components/button';
 import Input from './components/input';
+import ClearButton from './components/clearButton';
 import React, { Component } from "react";
 import './App.css';
-import { render } from '@testing-library/react';
+
+import * as math from "mathjs";
 
 class App extends Component{
 
@@ -14,34 +16,53 @@ class App extends Component{
     }
 
   }
+
+  //Take button number set it to input state
+  addToInput = val =>{
+    this.setState({input: this.state.input + val});
+  }
+  
+  //Handle the = to evaluate math from pakage mathjs
+  handleEqual = () => {
+    this.setState({ input: math.evaluate(this.state.input) });
+  }
+
   render() {
     return (
       <div className="App">
         <div className="calc-wrapper">
             <Input input={this.state.input}></Input>
+
           <div className="row">
-            <Button>7</Button>
-            <Button>8</Button>
-            <Button>9</Button>
-            <Button>/</Button>   
+            <Button handleClick={this.addToInput}>7</Button>
+            <Button handleClick={this.addToInput}>8</Button>
+            <Button handleClick={this.addToInput}>9</Button>
+            <Button handleClick={this.addToInput}>/</Button>   
           </div>
+
           <div className="row">
-            <Button>4</Button>
-            <Button>5</Button>
-            <Button>6</Button>
-            <Button>X</Button>   
+            <Button handleClick={this.addToInput}>4</Button>
+            <Button handleClick={this.addToInput}>5</Button>
+            <Button handleClick={this.addToInput}>6</Button>
+            <Button handleClick={this.addToInput}>*</Button>   
           </div>
+
           <div className="row">
-            <Button>1</Button>
-            <Button>2</Button>
-            <Button>3</Button>
-            <Button>+</Button>   
+            <Button handleClick={this.addToInput}>1</Button>
+            <Button handleClick={this.addToInput}>2</Button>
+            <Button handleClick={this.addToInput}>3</Button>
+            <Button handleClick={this.addToInput}>+</Button>   
           </div>
+
           <div className="row">
-            <Button>.</Button>
-            <Button>0</Button>
-            <Button>=</Button>
-            <Button>-</Button>   
+            <Button handleClick={this.addToInput}>.</Button>
+            <Button handleClick={this.addToInput}>0</Button>
+            <Button handleClick={() => this.handleEqual()}>=</Button>
+            <Button handleClick={this.addToInput}>-</Button>   
+          </div>
+          
+          <div className="row">
+            <ClearButton handleClear={() => this.setState({ input: "" })}>Clear</ClearButton> {/* Clear out the input to empty */}
           </div>
         </div>
       </div>
--- src/components/button.jsx
+++ src/components/button.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import "./button.css";
 
+//Determ if isnt a number and . = sign to evaluate diffrent style
 const isOperator = val => {
     return !isNaN(val) || val === "." || val === "=";
 };
 
 const Button = props => (
-    <div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`}>
+    <div className={`button-wrapper ${isOperator(props.children) ? null : "operator"}`} onClick={() => props.handleClick(props.children)}>
         {props.children}
     </div>
 );
